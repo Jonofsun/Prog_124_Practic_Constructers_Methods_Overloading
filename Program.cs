24e5f5d2e29c58c7b76067bd3ea5ab2d0f2481b5
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2023-04-18 15:21:51 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_124_Practic_Constructers_Methods_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
